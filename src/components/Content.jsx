2d3fa0fb6d06d11e398bf3c5d80b0846b9925f26
5gMurilo <cardoso.murilo2002@gmail.com>
2021-02-18 18:52:56 -0300

    commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,24 +6,29 @@ import hospital from "../img/hospital.png";
 
 export const Content = (_) => {
   return (
-    <div className="grid grid-cols-2 mx-14 my-11">
+    <div className="grid grid-cols-2 mx-16 my-14">
       {/* total de casos */}
-      <div className="box-content flow-root h-44 p-2 my-4 mx-10 flex bg-gradient-to-tl from-azure to-skyblue">
+      <div className="box-content flow-root h-44 p-2 my-4 mx-10 flex bg-gradient-to-tl from-azure to-skyblue shadow-lg">
         <div className="flex h-14 w-96 inline-block ml-2">
-          <img src={brasil} className="h-12 self-center" />
+          <img src={brasil} className="h-12 self-center " />
           <span className="font-bold text-xl p-0.5 self-center h-auto  leading-none m-2 object-fill tracking-wide text-white">
             Total de casos
           </span>
         </div>
 
         {/* dados */}
-        <div className="flex h-14 w-96 inline-block ml-21">
-          <span>xxx.xxx.xxx.xxx</span>
+        <div className="flex h-14 w-full inline-block mt-4 align-middle justify-center">
+          <span
+            className="self-center h-auto leading-none font-semibold text-2xl text-white"
+            id="tot"
+          >
+            xxx.xxx.xxx.xxx
+          </span>
         </div>
       </div>
 
       {/* casos ativos */}
-      <div className="box-content flow-root h-44 p-2 my-4 mx-10 flex bg-gradient-to-tl from-sunglow to-atomicTangerine">
+      <div className="box-content flow-root h-44 p-2 my-4 mx-10 flex bg-gradient-to-tl from-sunglow to-atomicTangerine shadow-lg">
         <div className="flex h-14 w-96 inline-block ml-2">
           <img src={atenção} className="h-12 self-center" />
           <span className="font-bold text-2xl p-0.5 self-center h-auto  leading-none m-2 object-fill tracking-wide text-white">
@@ -32,15 +37,18 @@ export const Content = (_) => {
         </div>
 
         {/* dados */}
-        <div className="flex h-14 w-full inline-block ml-21 align-middle justify-center">
-          <span className="self-center h-auto leading-none font-semibold text-xl">
+        <div className="flex h-14 w-full inline-block mt-4 align-middle justify-center">
+          <span
+            className="self-center h-auto leading-none font-semibold text-2xl text-white"
+            id="act"
+          >
             xxx.xxx.xxx.xxx
           </span>
         </div>
       </div>
 
       {/* casos recuperados */}
-      <div className="box-content flow-root h-44 p-2 my-4 mx-10 flex bg-gradient-to-tl from-seaGreenCrayola via-mediumAquamarine to-fluorescentBlue">
+      <div className="box-content flow-root h-44 p-2 my-4 mx-10 flex bg-gradient-to-tl from-seaGreenCrayola via-mediumAquamarine to-fluorescentBlue shadow-lg">
         <div className="flex h-14 w-96 inline-block ml-2">
           <img src={hospital} className="h-12 self-center" />
           <span className="font-bold text-xl p-0.5 self-center h-auto  leading-none m-2 object-fill tracking-wide text-white">
@@ -49,13 +57,18 @@ export const Content = (_) => {
         </div>
 
         {/* dados */}
-        <div className="flex h-14 w-96 inline-block ml-21">
-          <span>xxx.xxx.xxx.xxx</span>
+        <div className="flex h-14 w-full inline-block mt-4 align-middle justify-center">
+          <span
+            className="self-center h-auto leading-none font-semibold text-2xl text-white"
+            id="rec"
+          >
+            xxx.xxx.xxx.xxx
+          </span>
         </div>
       </div>
 
       {/* mortes */}
-      <div className="box-content flow-root h-44 p-2 my-4 mx-10 flex bg-gradient-to-tl from-charlestonGreen to-Onyx">
+      <div className="box-content flow-root h-44 p-2 my-4 mx-10 flex bg-gradient-to-tl from-charlestonGreen to-Onyx shadow-lg">
         <div className="flex h-14 w-96 inline-block ml-2">
           <img src={caveira} className="h-12 self-center" />
           <span className="font-bold text-xl p-0.5 self-center h-auto  leading-none m-2 object-fill tracking-wide text-white">
@@ -64,8 +77,13 @@ export const Content = (_) => {
         </div>
 
         {/* dados */}
-        <div className="flex h-14 w-96 inline-block ml-21">
-          <span>xxx.xxx.xxx.xxx</span>
+        <div className="flex h-14 w-full inline-block mt-4 align-middle justify-center">
+          <span
+            className="self-center h-auto leading-none font-semibold text-2xl text-white"
+            id="mor"
+          >
+            xxx.xxx.xxx.xxx
+          </span>
         </div>
       </div>
     </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 const Header = (_) => {
   return (
-    <header className="w-screen h-auto p-3">
-      <div>
-        <p className="text-center font-sans text-2xl font-medium tracking-wide">
-          Status da covid-19 no Brasil
-        </p>
-      </div>
+    <header className="w-screen h-20 align-middle justify-center">
+      <p className="text-center font-sans text-2xl font-medium tracking-wide">
+        Status da covid-19 no Brasil
+      </p>
     </header>
   );
 };
