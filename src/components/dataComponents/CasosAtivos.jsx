f388042701ac6fdc6687d1ef1b783ecda9ea839f
Murilo Cardoso <65872392+5gMurilo@users.noreply.github.com>
2021-02-21 00:18:59 -0300

    commit

---
+++ src/components/dataComponents/CasosAtivos.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CasosAtivos = (_) => {
+  return (
+    <span
+      className="self-center h-auto leading-none font-semibold text-2xl text-white"
+      id="act"
+    >
+      xxx.xxx.xxx.xxx
+    </span>
+  );
+};
+
+export default CasosAtivos;
---
+++ src/components/dataComponents/TotalCasos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TotalCasos = (_) => {
+  return (
+    <span className="self-center h-auto leading-none font-semibold text-2xl text-white">
+      xxx.xxx.xxx.xxx
+    </span>
+  );
+};
+
+export default TotalCasos;
