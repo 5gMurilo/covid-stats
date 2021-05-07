d115be8d83ef40f3ffd8666149824f84da4d0ac3
Murilo Cardoso <cardoso.murilo2002@gmail.com>
2021-05-07 10:32:26 -0300

    commit

--- src/components/server/api.js
+++ src/components/server/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  url: "https://api.covid19api.com/live/country/brazil/status/confirmed",
-  headers: {},
+  method: "GET",
+  url: "https://covid-193.p.rapidapi.com/countries",
+  headers: {
+    "x-rapidapi-key": "227fc38c13msh95037b604a11f7ep15fbb7jsn580a81f4bf6d",
+    "x-rapidapi-host": "covid-193.p.rapidapi.com",
+  },
 });
 
 export default api;
